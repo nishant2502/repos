59b9e827117295fe55f81f401f2abc4475d23bd7
nishant2502 <80115029+nishant2502@users.noreply.github.com>
2022-01-31 15:56:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+noseX=0
+noseY=0
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+    video.position(100,250);
+    canvas = createCanvas(560, 550);
+    canvas.position(800, 250);
+    PoseNet = ml5.poseNet(video, modelLoaded);
+    PoseNet.on('pose', gotPoses);
+}
+function modelLoaded() {
+    console.log('PoseNet Is Initialized!');
+}
+function draw() {
+    background('#969A97');
+
+    document.getElementById("square_side").innerHTML = "Width And Height Of A Square Will Be = " + difference +"px";
+    fill('#F90093');
+    stroke('#F90093');
+    square(noseX, noseY, difference);
+}
+function gotPoses(results) {
+    if (results.length > 0) 
+    {
+        console.log(results);
+noseX = results[0].pose.nose.x;
+noseY = results[0].pose.nose.y;
+console.log("noseX = " + noseX +" noseY = " + noseY);
+
+leftWristX = results[0].pose.leftWrist.x;
+rightWristX = results[0].pose.rightWrist.x;
+difference = floor(leftWristX - rightWristX);
+
+console.log("leftWristX =" + leftWristX + " rightWristX = "+ rightWristX + " difference = " + difference);
+    }
+}
